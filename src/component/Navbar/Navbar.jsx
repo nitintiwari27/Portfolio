3868d784a41a9f818c5e33c18ddc1bc95de1a2a5
nitintiwari27 <nitintiwari5111@gmail.com>
2025-11-01 16:48:25 +0530

    Done

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import NTLogo from "../../assets/NTLOGO.png";
+import NTLogo from "../../assets/logo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
